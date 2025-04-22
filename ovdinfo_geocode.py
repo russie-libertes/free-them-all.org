5be4c49b9ac0bc4f2fa27a08b0ee0c7f69f3be79
Vadim Kantorov <vadimkantorov@gmail.com>
2025-04-22 23:54:57 +0300

    ...

--- ovdinfo_geocode.py
+++ ovdinfo_geocode.py
@@ -1,4 +1,5 @@
 import os
+import csv
 import json
 import argparse
 import urllib.request
@@ -11,7 +12,7 @@ parser.add_argument('--verbose-http', action = 'store_true')
 parser.add_argument('--ovdinfo-dataset-url', default = 'https://api.repression.info/v1/data')
 parser.add_argument('--google-geocoding-api-url', default = 'https://maps.googleapis.com/maps/api/geocode/json?address={ADDRESS}&key={GOOGLE_API_KEY}', help = 'https://developers.google.com/maps/documentation/urls/get-started')
 parser.add_argument('--google-places-api-url', default = 'https://places.googleapis.com/v1/places:searchText', help = 'https://developers.google.com/maps/documentation/places/web-service/text-search')
-parser.add_argument('--cache-json-path')
+parser.add_argument('--cache-path')
 parser.add_argument('--cache-override', action = 'store_true')
 args = parser.parse_args()
 
@@ -21,9 +22,19 @@ if args.dotenv and os.path.exists(args.dotenv):
 if args.google_api_key:
     secrets['GOOGLE_API_KEY'] = args.google_api_key
 cache = {}
-if args.cache_json_path and os.path.exists(args.cache_json_path):
-    with open(args.cache_json_path) as f:
-        cache.update(json.load(f))
+fieldnames = ['address', 'mapsurl']
+if args.cache_path and os.path.exists(args.cache_path):
+    if args.cache_path.endswith('.json'):
+        with open(args.cache_path) as f:
+            cache.update(json.load(f))
+    elif args.cache_path.endswith('.tsv'):
+        with open(args.cache_path) as f:
+            reader = csv.reader(f, delimiter = '\t', quoting = csv.QUOTE_NONE, quotechar='\t')
+            rows = list(reader)
+            if rows:
+                fieldnames = rows[0]
+                cache.update({row[0] : row for row in rows[1:]})
+print('before', cache)
 
 if args.verbose_http:
     import http.client
@@ -35,29 +46,42 @@ print('Total prisoners:', ovd['total'])
 for prisoner in ovd['data']:
     addresses = set(prisoner["detention_center_ru"]) | set([prisoner["restraint_measure_location_ru"]]) | set(prisoner["penal_facility_ru"]) | set([prisoner["imprisonment_location_ru"]])
     for address in filter(bool, addresses):
-        address = address.translate({ord('«') : ' ', ord('»') : ' ', ord('"') : ' ', ord("№") : ' ', ord('|') : ','}).replace('  ', ' ')
+        addresssafe = address.translate({ord('«') : ' ', ord('»') : ' ', ord('"') : ' ', ord("№") : ' ', ord('|') : ','}).replace('  ', ' ')
         if address not in cache or args.cache_override:
-            places = json.load(urllib.request.urlopen(urllib.request.Request(args.google_places_api_url, data = json.dumps(dict(textQuery = address)).encode('utf-8'), method = 'POST', headers = {'Content-Type': 'application/json', 'X-Goog-Api-Key' : secrets.get('GOOGLE_API_KEY', ''), 'X-Goog-FieldMask': 'places.id,places.displayName,places.formattedAddress,places.googleMapsUri,places.location'})))
-            print(places)
+            places = json.load(urllib.request.urlopen(urllib.request.Request(args.google_places_api_url, data = json.dumps(dict(textQuery = addresssafe)).encode('utf-8'), method = 'POST', headers = {'Content-Type': 'application/json', 'X-Goog-Api-Key' : secrets.get('GOOGLE_API_KEY', ''), 'X-Goog-FieldMask': 'places.id,places.displayName,places.formattedAddress,places.googleMapsUri,places.location'})))
+            print('Places:', len(places['places']))
+            for placeres in places['places']:
+                placeid = placeres['id']
+                lat, lng = map(placeres['location'].get, ['latitude', 'longitude'])
+                mapsurl1 = 'https://google.com/maps/search/?' + urllib.parse.urlencode(dict(api = '1', query = f'{lat},{lng}', query_place_id = placeid))
+                mapsurl2 = placeres['googleMapsUri']
+                if len(places['places']) == 1:
+                    cache[address] = [address, mapsurl2]
             
-            url = args.google_geocoding_api_url.format(GOOGLE_API_KEY = secrets.get('GOOGLE_API_KEY', ''), ADDRESS = urllib.parse.quote_plus(address) )
-            print(url)
-            geo = json.load(urllib.request.urlopen(url))
-
-            print('Geo status:', geo['status'], ', results:', len(geo['results']))
-            assert 'OK' == geo['status']
-            for geores in geo['results']:
-                print(address)
-                print(geores)
-                placeid = geores['place_id']
-                lat, lng = map(geores['geometry']['location'].get, ['lat', 'lng'])
-                mapsurl = 'https://google.com/maps/search/?' + urllib.parse.urlencode(dict(api = '1', query = f'{lat},{lng}', query_place_id = placeid))
-                print(mapsurl)
-                print()
-                if len(geo['results']) == 1:
-                    cache[address] = mapsurl
-            
+            print('after', cache)
+            if args.cache_path:
+                if args.cache_path.endswith('.json'):
+                    json.dump(open(args.cache_path, 'w'), cache, ensure_ascii = False, indent = 2, sort_keys = True)
+                elif args.cache_path.endswith('.tsv'):
+                    with open(args.cache_path, 'w') as f:
+                        writer = csv.writer(f, delimiter = '\t', quoting = csv.QUOTE_NONE, quotechar='\t')
+                        writer.writerow(fieldnames)
+                        writer.writerows(cache.values())
             import sys; sys.exit(0)
+            
+            #url = args.google_geocoding_api_url.format(GOOGLE_API_KEY = secrets.get('GOOGLE_API_KEY', ''), ADDRESS = urllib.parse.quote_plus(addresssafe) )
+            #print(url)
+            #geo = json.load(urllib.request.urlopen(url))
+            #print('Geo status:', geo['status'], ', results:', len(geo['results']))
+            #assert 'OK' == geo['status']
+            #for geores in geo['results']:
+            #    print(address)
+            #    print(geores)
+            #    placeid = geores['place_id']
+            #    lat, lng = map(geores['geometry']['location'].get, ['lat', 'lng'])
+            #    mapsurl = 'https://google.com/maps/search/?' + urllib.parse.urlencode(dict(api = '1', query = f'{lat},{lng}', query_place_id = placeid))
+            #    print(mapsurl)
+            #    print()
+            #    if len(geo['results']) == 1:
+            #        cache[address] = mapsurl
 
-if args.cache_json_path:
-    json.dump(open(args.cache_json_path, 'w'), cache, ensure_ascii = False, indent = 2, sort_keys = True)
