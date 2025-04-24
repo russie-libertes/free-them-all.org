da2568a38f7ab04c860d6b18c9a72f8684d681d4
Vadim Kantorov <vadimkantorov@gmail.com>
2025-04-24 15:46:36 +0300

    ...

---
+++ gen.py
@@ -0,0 +1,23 @@
+import argparse
+import csv
+import json
+import urllib.request
+import urllib.parse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--ovd-info-dataset-url', default = 'https://api.repression.info/v1/data')
+parser.add_argument('--ovd-info-addresses-csv', default = 'data/prisons_ovd.csv')
+parser.add_argument('--output-json', default = 'data.json')
+args = parser.parse_args()
+
+ovd = json.load(urllib.request.urlopen(args.ovd_info_dataset_url))
+print('Total prisoners:', ovd['total'])
+print('First prisoner:', ovd['data'][0])
+
+addresses = {row[0] : dict(name = row[0], mapmarkerkey = row[0], address = row[1], latlon = row[-1], url = 'https://google.com/maps/search/?' + urllib.parse.urlencode(dict(api = '1', query = row[-1]))) for row in list(csv.reader(open(args.ovd_info_addresses_csv)))[1:]}
+
+sanitize_addr = lambda addr: addr.translate({ord('"') : '', ord('\u00A0'): ' ', ord('\xa0') : ' ', ord('\r'): ' ', ord('\n'): ' '}).strip()
+   
+data = [addresses[sanitized_addr] for prisoner in ovd['data'] for addr in set(prisoner["detention_center_ru"]) | set([prisoner["restraint_measure_location_ru"]]) | set(prisoner["penal_facility_ru"]) | set([prisoner["imprisonment_location_ru"]]) for sanitized_addr in filter(bool, [sanitize_addr(addr)]) if sanitized_addr in addresses] 
+
+json.dump(data, open(args.output_json, 'w'), ensure_ascii = False, indent = 2)
--- ovdinfo_geocode.py
+++ ovdinfo_geocode.py
@@ -12,7 +12,7 @@ parser.add_argument('--verbose-http', action = 'store_true')
 parser.add_argument('--ovdinfo-dataset-url', default = 'https://api.repression.info/v1/data')
 parser.add_argument('--google-geocoding-api-url', default = 'https://maps.googleapis.com/maps/api/geocode/json?address={ADDRESS}&key={GOOGLE_API_KEY}', help = 'https://developers.google.com/maps/documentation/urls/get-started')
 parser.add_argument('--google-places-api-url', default = 'https://places.googleapis.com/v1/places:searchText', help = 'https://developers.google.com/maps/documentation/places/web-service/text-search')
-parser.add_argument('--cache-path')
+parser.add_argument('--cache-tsv-path')
 parser.add_argument('--cache-override', action = 'store_true')
 args = parser.parse_args()
 
@@ -22,18 +22,27 @@ if args.dotenv and os.path.exists(args.dotenv):
 if args.google_api_key:
     secrets['GOOGLE_API_KEY'] = args.google_api_key
 cache = {}
-fieldnames = ['address', 'mapsurl']
-if args.cache_path and os.path.exists(args.cache_path):
-    if args.cache_path.endswith('.json'):
-        with open(args.cache_path) as f:
-            cache.update(json.load(f))
-    elif args.cache_path.endswith('.tsv'):
-        with open(args.cache_path) as f:
+fieldnames = ['address', 'latitude', 'longitude', 'ok', 'mapsurl']
+
+def save_cache(cache):
+    if args.cache_tsv_path:
+        rows = cache.values()
+        with open(args.cache_tsv_path, 'w') as f:
+            writer = csv.writer(f, delimiter = '\t', quoting = csv.QUOTE_NONE, quotechar='\t')
+            writer.writerow(fieldnames)
+            writer.writerows(rows)
+
+def load_cache(cache, fieldnames):
+    if args.cache_tsv_path and os.path.exists(args.cache_tsv_path):
+        with open(args.cache_tsv_path) as f:
             reader = csv.reader(f, delimiter = '\t', quoting = csv.QUOTE_NONE, quotechar='\t')
             rows = list(reader)
             if rows:
                 fieldnames = rows[0]
                 cache.update({row[0] : row for row in rows[1:]})
+    return cache, fieldnames
+
+cache, fieldnames = load_cache(cache, fieldnames)
 
 if args.verbose_http:
     import http.client
@@ -45,17 +54,23 @@ print('Total prisoners:', ovd['total'])
 for prisoner in ovd['data']:
     addresses = set(prisoner["detention_center_ru"]) | set([prisoner["restraint_measure_location_ru"]]) | set(prisoner["penal_facility_ru"]) | set([prisoner["imprisonment_location_ru"]])
     for address in filter(bool, addresses):
-        addresssafe = address.translate({ord('«') : ' ', ord('»') : ' ', ord('"') : ' ', ord("№") : ' ', ord('|') : ','}).replace('  ', ' ')
-        if address not in cache or args.cache_override:
-            places = json.load(urllib.request.urlopen(urllib.request.Request(args.google_places_api_url, data = json.dumps(dict(textQuery = addresssafe)).encode('utf-8'), method = 'POST', headers = {'Content-Type': 'application/json', 'X-Goog-Api-Key' : secrets.get('GOOGLE_API_KEY', ''), 'X-Goog-FieldMask': 'places.id,places.displayName,places.formattedAddress,places.googleMapsUri,places.location'})))
-            print('Places:', len(places['places']))
-            for placeres in places['places']:
+        addresssafe = address.translate({ord('«') : ' ', ord('»') : ' ', ord('"') : ' ', ord("№") : ' ', ord('|') : ',', ord('\n') : ' ', ord('\r') : ' '}).replace('  ', ' ')
+        if addresssafe not in cache or args.cache_override:
+            try:
+                placeres = json.load(urllib.request.urlopen(urllib.request.Request(args.google_places_api_url, data = json.dumps(dict(textQuery = addresssafe)).encode('utf-8'), method = 'POST', headers = {'Content-Type': 'application/json', 'X-Goog-Api-Key' : secrets.get('GOOGLE_API_KEY', ''), 'X-Goog-FieldMask': 'places.id,places.displayName,places.formattedAddress,places.googleMapsUri,places.location'})))
+            except Exception as e:
+                save_cache(cache)
+                print(e)
+                import sys; sys.exit(1)
+                
+            places = placeres.get('places', [])
+            print('Places:', len(places))
+            for placeres in places:
                 placeid = placeres['id']
                 lat, lng = map(placeres['location'].get, ['latitude', 'longitude'])
                 mapsurl1 = 'https://google.com/maps/search/?' + urllib.parse.urlencode(dict(api = '1', query = f'{lat},{lng}', query_place_id = placeid))
                 mapsurl2 = placeres['googleMapsUri']
-                if len(places['places']) == 1:
-                    cache[address] = [address, mapsurl2]
+                cache[addresssafe] = [addresssafe, str(lat), str(lng), '', mapsurl2]
             
             #url = args.google_geocoding_api_url.format(GOOGLE_API_KEY = secrets.get('GOOGLE_API_KEY', ''), ADDRESS = urllib.parse.quote_plus(addresssafe) )
             #print(url)
@@ -73,11 +88,4 @@ for prisoner in ovd['data']:
             #    if len(geo['results']) == 1:
             #        cache[address] = mapsurl
 
-if args.cache_path:
-    if args.cache_path.endswith('.json'):
-        json.dump(open(args.cache_path, 'w'), cache, ensure_ascii = False, indent = 2, sort_keys = True)
-    elif args.cache_path.endswith('.tsv'):
-        with open(args.cache_path, 'w') as f:
-            writer = csv.writer(f, delimiter = '\t', quoting = csv.QUOTE_NONE, quotechar='\t')
-            writer.writerow(fieldnames)
-            writer.writerows(cache.values())
+save_cache(cache)
