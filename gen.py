fab40703c0095217b2e4806287c105aea01b902b
Vadim Kantorov <vadimkantorov@gmail.com>
2025-04-24 15:53:30 +0300

    ...

--- gen.py
+++ gen.py
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--ovd-info-dataset-url', default = 'https://api.repression.info/v1/data')
 parser.add_argument('--ovd-info-addresses-csv', default = 'data/prisons_ovd.csv')
 parser.add_argument('--output-json', default = 'data.json')
+parser.add_argument('--output-js', default = 'data.js')
 args = parser.parse_args()
 
 ovd = json.load(urllib.request.urlopen(args.ovd_info_dataset_url))
@@ -21,3 +22,5 @@ sanitize_addr = lambda addr: addr.translate({ord('"') : '', ord('\u00A0'): ' ', 
 data = [addresses[sanitized_addr] for prisoner in ovd['data'] for addr in set(prisoner["detention_center_ru"]) | set([prisoner["restraint_measure_location_ru"]]) | set(prisoner["penal_facility_ru"]) | set([prisoner["imprisonment_location_ru"]]) for sanitized_addr in filter(bool, [sanitize_addr(addr)]) if sanitized_addr in addresses] 
 
 json.dump(data, open(args.output_json, 'w'), ensure_ascii = False, indent = 2)
+
+open(args.output_js, 'w').write('data = ' + json.dumps(data, ensure_ascii = False, indent = 2))
