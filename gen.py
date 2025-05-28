04dfd1492775b495c8be53a756fa53fcc5ce3459
Vadim Kantorov <vadimkantorov@gmail.com>
2025-05-28 14:31:06 +0200

    ...

--- gen.py
+++ gen.py
@@ -6,7 +6,7 @@ import urllib.request
 import urllib.parse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--ovd-info-dataset-url', default = 'https://api.repression.info/v1/data')
+parser.add_argument('--ovd-info-dataset-url', default = 'https://api.repression.info/v1/data', help = 'curl https://api.repression.info/v1/data | jq > ovdinfo.json')
 parser.add_argument('--ovd-info-addresses-csv', default = 'data/prisons_ovd.csv')
 parser.add_argument('--output-json', default = 'data.json')
 parser.add_argument('--output-js', default = 'data.js')
@@ -21,7 +21,7 @@ addresses = {row[0] : dict(name = row[0], mapmarkerkey = row[0], address = row[1
 
 sanitize_addr = lambda addr: addr.translate({ord('"') : '', ord('\u00A0'): ' ', ord('\xa0') : ' ', ord('\r'): ' ', ord('\n'): ' '}).strip()
    
-data = [dict(prisoner, **addresses[sanitized_addresses[0]]) for prisoner in ovd['data'] for sanitized_addresses in [list(filter(bool, map(sanitize_addr, prisoner["detention_center_ru"] +[prisoner["restraint_measure_location_ru"]] + prisoner["penal_facility_ru"] + [prisoner["imprisonment_location_ru"]])))] if sanitized_addresses and sanitized_addresses[0] in addresses] 
+data = [dict(prisoner, **addresses[sanitized_addresses[0]]) for prisoner in ovd['data'] for sanitized_addresses in [list(filter(bool, map(sanitize_addr, prisoner["detention_center_ru"] + [prisoner["persecution_location_ru"]] + prisoner["penal_facility_ru"] + [prisoner["imprisonment_location_ru"]])))] if sanitized_addresses and sanitized_addresses[0] in addresses] 
 
 os.makedirs(args.output_dir, exist_ok = True)
 for basename in ['index.html', 'leaflet.js', 'leaflet.css'] * (args.output_dir != '.'):
